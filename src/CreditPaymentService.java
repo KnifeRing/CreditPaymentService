cfbef857c1db48622fb735f0422884209b57977e
Alexander Khyrbu <rjrednaxela@vk.ru>
2024-03-05 23:34:10 +0300

    ed logic

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -3,9 +3,9 @@ public class CreditPaymentService {
     public int calculate(int total, int years, double percent){
         double monthlyPercent = percent / (12*100); // месячная процентная ставка 12 это - кол-во
                                                     // месяцев, 100 процент
-        double sumMonth = years * 12; // общее кол-во месяцев
+        double Month = years * 12; // общее кол-во месяцев
 
-        double monthlyPayment = total * monthlyPercent / (1 - Math.pow(1 + monthlyPercent, - sumMonth));
+        double monthlyPayment = total * monthlyPercent / (1 - Math.pow(1 + monthlyPercent, - Month));
 
         return (int) monthlyPayment;
     }
